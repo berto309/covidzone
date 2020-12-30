babe946862054912311900862324ded56270dcd8
Robert Ntim <ntimrobert309@gmail.com>
2020-12-30 12:35:47 +0000

    wip

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,20 @@
 const btnHamburger = document.querySelector('#btnHamburger');
-   
+const landing = document.querySelector('.landing');
+const overlay = document.querySelector('.overlay')
+
+
 
 btnHamburger.addEventListener('click', function () {
-    if (btnHamburger.classList.contains('open')) {
-       btnHamburger.classList.remove('open')
-    } else {
-        btnHamburger.classList.add('open')
+   if (landing.classList.contains('open')) { 
+      //  close hamburger menu
+      landing.classList.remove('open')
+       overlay.classList.remove('fade-in');
+       overlay.classList.add('fade-out');
+   } else {
+      // open hamburger menu
+      landing.classList.add('open')
+      overlay.classList.remove('fade-out');
+      overlay.classList.add('fade-in');
    }
 })
  
